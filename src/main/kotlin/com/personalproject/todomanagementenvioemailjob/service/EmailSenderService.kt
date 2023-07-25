5ee9144ead159c164e44cf5223ea1f19b36b016c
Rafael Rodrigues da Silva <rafaelx7t@hotmail.com>
2023-07-25 18:49:12 -0300

    Correção formato HTML no e-mail

--- src/main/kotlin/com/personalproject/todomanagementenvioemailjob/service/EmailSenderService.kt
+++ src/main/kotlin/com/personalproject/todomanagementenvioemailjob/service/EmailSenderService.kt
@@ -1,22 +1,24 @@
 package com.personalproject.todomanagementenvioemailjob.service
 
 import com.personalproject.todomanagementenvioemailjob.model.Email
+import jakarta.mail.internet.MimeMessage
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.mail.MailSender
-import org.springframework.mail.SimpleMailMessage
+import org.springframework.mail.javamail.JavaMailSender
+import org.springframework.mail.javamail.MimeMessageHelper
 import org.springframework.stereotype.Component
 
 @Component
 class EmailSenderService {
 
     @Autowired
-    private lateinit var emailSender: MailSender
+    private lateinit var emailSender: JavaMailSender
 
     fun send(email: Email) {
-        val message: SimpleMailMessage = SimpleMailMessage()
-        message.subject = email.title
-        message.text = email.text
-        message.setTo(email.receiver?.email)
+        val message: MimeMessage = emailSender.createMimeMessage()
+        val helper = MimeMessageHelper(message, "utf-8")
+        helper.setSubject(email.title)
+        helper.setText(email.text, true)
+        helper.setTo(email.receiver!!.email)
         emailSender.send(message)
     }
 }
